aa55c8fb0bebe42048ad8667c044dd90c2af2e12
Brooke Lewis <belv2c@gmail.com>
2018-04-02 19:28:15 -0500

    created orderPizzaService

---
+++ WebApplication2/Services/OrderPizzaService.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebApplication2.Models;
+
+namespace WebApplication2.Services
+{
+    public class OrderPizzaService
+    {
+        public bool OrderPizza(Order order)
+        {
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["PizzaTime"].ConnectionString))
+            {
+                db.Open();
+
+                var numberAffected = db.Execute("Insert into Orders(TypeOfPizza, NumberOfPizzas, Cost, AddressForDelivery, NameOfCustomer)" +
+                               "Values(@TypeOfPizza, @NumberOfPizzas, @Cost, @AddressForDelivery, @NameOfCustomer)",
+                                order);
+
+                return numberAffected == 1;
+            }
+        }
+    }
+}
